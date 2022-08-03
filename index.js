2903cd1db1763a79f1e3c710e96738800e4dd071
alizm3558 <alibakir3558@gmail.com>
2022-08-03 15:15:16 +0300

    Mongodb bağlantısı gerçekleştirildi.

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const express=require('express');
-
+const productsRouter=require("./routes/products");
 const app=express();
 
-app.get("/",(req,res)=>{ //req: gelen istek, res: gönderilen
-    res.send("Hello world");
-});
+const mongoose=require("mongoose");
+const username="<username>"
+const password="<password>"
+
+mongoose.connect('<connect_info>',
+(e)=>{
+    if(e){
+        console.log(e);
+        console.log("bakınız buradayız")
+    }
+    else
+    {console.log("Connected database"); 
+    } 
+                 
+}
+);
 
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 })
+
+app.use("/products",productsRouter);
+
+const products=[
+    {
+        id:"string",
+        title:"string",
+        desciption:"string",
+        prices:"number"
+    },
+]; 
