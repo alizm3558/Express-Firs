feb0b909bc24bcaf29e38268c61454d547240416
alizm3558 <alibakir3558@gmail.com>
2022-08-04 10:38:06 +0300

    rest methodlar eklendi.

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,34 @@
-const express=require('express');
+const express = require('express');
 
-const router=express.Router();
+const router = express.Router();
 
-router.get("/",(req,res)=>{
-    res.send("products");
+//route tanımlamalar
+router.get("/", (req, res) => {
+    res.send("fetch products list");
 });
 
-module.exports=router;
+
+router.get("/:id", (req, rest) => {
+    rest.send(`fetch product ${req.params.id}`);
+});
+
+//post
+router.post("/", (req, res) => {
+    res.send("create product");
+});
+
+router.put("/:id", (res, req) => {
+    res.send(`update product ${req.params.id}`);
+})
+
+router.delete("/:id", (req, rest) => {
+    rest.send(`delete product ${req.params.id}`);
+});
+
+
+module.exports = router;
+
+//not: mongodbdeki ip yi değiştir. sabit ip olması daha iyi.
+
+
+
