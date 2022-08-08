288f78e0d1bda96fed2072b3d880af45135a3b04
alizm3558 <alibakir3558@gmail.com>
2022-08-08 10:27:02 +0300

    ekleme yapıldı.

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,19 @@
 const express=require('express');
 
 const router=express.Router();
-
+const User=require("../models/Users");
 
 router.post("/register",(req,res)=>{
-    res.send("register");
+
+   const {name,email,password}=req.body;
+   const user=new User({name,email,password});
+   user
+   .save()
+   .then((user)=>{
+       res.send(user);
+   })
+   .catch((err)=>{});
+
 });
 
 router.post("/login",(req,res)=>{
